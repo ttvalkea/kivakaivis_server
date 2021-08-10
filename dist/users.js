c62eae70603c8556bf6fcda5318e1d58620d365e
Tuomas Valkeapää <tuomas.valkeapaa@gmail.com>
2021-08-10 16:41:53 +0300

    Added socket.io

---
+++ dist/users.js
@@ -0,0 +1,26 @@
+const users = [];
+// joins the user to the specific chatroom
+function joinUser(id, userName, room) {
+    const user = { id, userName, room };
+    users.push(user);
+    console.log(users, "users");
+    return user;
+}
+console.log("user out", users);
+// Gets a particular user id to return the current user
+function getCurrentUser(id) {
+    return users.find((u) => u.id === id);
+}
+// called when the user leaves the chat and its user object deleted from array
+function userDisconnect(id) {
+    const index = users.findIndex((u) => u.id === id);
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+module.exports = {
+    joinUser,
+    getCurrentUser,
+    userDisconnect,
+};
+//# sourceMappingURL=users.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,77 @@
-import express from "express";
+// import express from "express";
 
+// const app = express();
+// const port = 3001;
+// app.get("/", (req, res) => {
+//   res.send("Response from kivakaivis_server");
+// });
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+import express from "express";
+import cors from "cors";
+const socket = require("socket.io");
 const app = express();
-const port = 3001;
-app.get("/", (req, res) => {
-  res.send("Response from kivakaivis_server");
-});
-app.listen(port, () => {
+app.use(express());
+
+const port = 8000;
+
+app.use(cors());
+
+var server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const io = socket(server);
+
+//initializing the socket io connection
+io.on("connection", (socket) => {
+  //for a new user joining the room
+  socket.on("joinRoom", ({ userName, roomName }) => {
+    //* create user
+    const user = joinUser(socket.id, userName, roomName);
+    console.log(socket.id, "=id");
+    socket.join(user.room);
+
+    //display a welcome message to the user who have joined a room
+    socket.emit("message", {
+      userId: user.id,
+      username: user.userName,
+      text: `Welcome ${user.userName}`,
+    });
+
+    //displays a joined room message to all other room users except that particular user
+    socket.broadcast.to(user.room).emit("message", {
+      userId: user.id,
+      username: user.userName,
+      text: `${user.userName} has joined the chat`,
+    });
+  });
+
+  //user sending message
+  socket.on("chat", (text) => {
+    //gets the room user and the message sent
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", {
+      userId: user.id,
+      userName: user.userName,
+      text: text,
+    });
+  });
+
+  //when the user exits the room
+  socket.on("disconnect", () => {
+    //the user is deleted from array of users and a left room message displayed
+    const user = userDisconnect(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        userId: user.id,
+        username: user.username,
+        text: `${user.username} has left the room`,
+      });
+    }
+  });
 });
